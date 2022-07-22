d360d44b29b16542d93bdaa889ef992c12d0cea7
safayet-arman <safayetarman11@gmail.com>
2022-07-22 21:46:59 +0600

    first commit

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">Mouatasim&Arman</span>
+          <span className="logo">armanmoatasim</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
@@ -21,7 +21,7 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img src="https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/292231888_578274930491792_2702368483221974972_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF2_8RQA5QObKA9BTEdgxK9EdN3UPPe7ZQR03dQ897tlHiQgn1fN2iJwTLGGL-sHp9Zrv9Nr22QC-AwbRzwgCnX&_nc_ohc=p4g91RNsGw4AX9V6DTA&_nc_ht=scontent.fdac31-1.fna&oh=00_AT80Sm7DIsT0U_oPibtW92p6Jdy93P5D9HJTzOWmfcfPDQ&oe=62DFCCC8" alt="" className="topAvatar" />
         </div>
       </div>
     </div>
